e9c9d8898de4899f8057ea7200238e286177a747
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-07-30 13:55:28 +0300

    Create comment class

--- comments.py
+++ comments.py
@@ -1,7 +1,25 @@
-comments = []
-current_comment_id = 0
+# Comment class
+class Comment:
+    comments = []
+    current_id = 0
 
-replies = []
-current_reply_id = 0
+    def list(self):
+        return Comment.comments
 
-# functions
+    def add(self, body):
+        Comment.current_id += 1
+        comment = {
+            'id': Comment.current_id,
+            'body': body
+        }
+        Comment.comments.append(comment)
+        return comment
+
+    def delete(self, comment_id):
+        # check if user can delete
+        for i, comment in enumerate(Comment.comments):
+            if comment['id'] == comment_id:
+                del Comment.comments[i]
+        return True
+
+# Reply class
