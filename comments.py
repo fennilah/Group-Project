1e3797e52e740b3e526d2c5d03aedefb35c6d512
Fennilah <fennymwikali@gmail.com>
2018-07-30 14:30:14 +0300

    Reply Class

--- comments.py
+++ comments.py
@@ -1,7 +1,20 @@
-comments = []
-current_comment_id = 0
+class Reply:
+    replies = []
+    current_id = 0
 
-replies = []
-current_reply_id = 0
-
-# functions
+    def add(self, comment_id, body):
+        """
+        Add a comment reply
+        """
+        # increment the id - for uniqueness
+        Reply.current_id += 1
+        # create reply
+        reply = {
+            'id': Reply.current_id,
+            'comment_id': comment_id,
+            'body': body
+        }
+        # append reply to list of replies
+        Reply.replies.append(reply)
+        # return the reply
+        return reply
